1382842a6850762112672ac26ee349e34fc84f50
Coniosorior <cosorio.riquelme@gmail.com>
2026-01-31 18:06:58 -0300

    avancemenu

--- menu.py
+++ menu.py
@@ -1,3 +1,35 @@
+ROLES = ("Admin", "Usuario", "Invitado")
+
+usuarios = [
+    {"user": "admin", "pass": "1234", "rol": "Admin", "mail": "admin@tienda.cl"},
+    {"user": "usuario", "pass": "5678", "rol": "Usuario", "mail": "usuario@tienda.cl"},
+    {"user": "invitado", "pass": "9123", "rol": "Invitado", "mail": "invitado@tienda.cl"},
+]
+
+mails_unicos = set()
+for u in usuarios:
+    mail = u.get("mail")
+    if mail in mails_unicos:
+        print(f"Mail duplicado detectado: {mail}")
+    else:
+        mails_unicos.add(mail)
+
+def login(usuarios):
+    print("\n===== LOGIN =====")
+
+    while True:
+        user_in = input("Usuario: ").strip()
+        pass_in = input("Contraseña: ").strip()
+
+        for u in usuarios:
+            if u.get("user") == user_in and u.get("pass") == pass_in:
+                print(f"\nBienvenido, {u.get('user')} (Rol: {u.get('rol')})")
+                return u
+
+        print("Usuario o contraseña incorrectos. Intenta nuevamente.")
+
+
+
 def mostrar_menu():
     print("\n===== TIENDA DE ALIMENTO PARA MASCOTAS =====")
     print("1) Agregar producto")
@@ -283,6 +315,9 @@ def main():
     eliminados = []
 
     ids_usados = {p["id"] for p in productos}
+
+    usuario_actual = login(usuarios)
+    rol_actual = usuario_actual.get("rol")
 
     while True:
         mostrar_menu()
